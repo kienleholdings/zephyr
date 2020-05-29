d17a023c9a191836e848b7182a2055b79cc1098f
jrkienle <james.kienle@gmail.com>
2020-05-28 18:46:32 -0700

    Add a script that allows us to compile a Zeplin Tailwind Config

---
+++ packages/zephyr-tailwind/generate-zeplin-config.js
@@ -0,0 +1,8 @@
+// We use the tailwind Zeplin Extension (https://extensions.zeplin.io/morrislaptop/tailwind-zeplin-extension)
+// to improve designer-developer handoff. This should be run at the end of every zephyr-tailwind
+// release to ensure developers have the most up-to-date class names
+
+const fs = require('fs');
+const transpilledConfig = require('./lib/tailwind').default;
+
+fs.writeFileSync('./lib/zeplin-config.json', JSON.stringify(transpilledConfig, null, 1));
