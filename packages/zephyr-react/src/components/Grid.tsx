b798542d969fe929cf1cd5c5bd7cd119c80c428d
jrkienle <james.kienle@gmail.com>
2020-07-07 17:01:45 -0700

    Create Type Defs and Cut Release of zephyr-react

--- packages/zephyr-react/src/components/Grid.tsx
+++ packages/zephyr-react/src/components/Grid.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import classNames from 'classnames';
 
 interface Props {
+  className?: string;
   noGutter?: boolean;
 }
 
-const Grid: React.FC<Props> = ({ children, noGutter }) => (
-  <div className={classNames('flex', { '-mx-16': !noGutter })}>{children}</div>
+const Grid: React.FC<Props> = ({ children, className, noGutter }) => (
+  <div className={classNames('flex', { '-mx-16': !noGutter }, className)}>{children}</div>
 );
 
 export default Grid;
